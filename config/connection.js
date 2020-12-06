6c3c1ce744e40e86f3046850fb9a2135c21d324f
Christopher Morrow <morrow7564@gmail.com>
2020-12-06 18:48:28 -0500

    adding jaws

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+console.log(process.env.NODE_ENV)
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Root",
+//   database: "burgers_db"
+// });
+
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "ixnzh1cxch6rtdrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Root",
-  database: "burgers_db"
+  user: "mladqyn1uzqpi549",
+  password: "lrhadtm8j36c1qoe",
+  database: "i9rjjbuzzbtp8xfp"
 });
 
 // Make connection.
