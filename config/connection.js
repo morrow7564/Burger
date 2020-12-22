527936fa0a6e90e9cfe1c2d0f026854034998871
Christopher Morrow <morrow7564@gmail.com>
2020-12-21 21:23:28 -0500

    fixing connection

--- config/connection.js
+++ config/connection.js
@@ -3,21 +3,21 @@ var mysql = require("mysql");
 
 console.log(process.env.NODE_ENV)
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Root",
-  database: "burgers_db"
-});
-
 // var connection = mysql.createConnection({
-//   host: "ixnzh1cxch6rtdrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   host: "localhost",
 //   port: 3306,
-//   user: "mladqyn1uzqpi549",
-//   password: "lrhadtm8j36c1qoe",
-//   database: "i9rjjbuzzbtp8xfp"
+//   user: "root",
+//   password: "Root",
+//   database: "burgers_db"
 // });
+
+var connection = mysql.createConnection({
+  host: "ixnzh1cxch6rtdrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "mladqyn1uzqpi549",
+  password: "lrhadtm8j36c1qoe",
+  database: "i9rjjbuzzbtp8xfp"
+});
 
 // Make connection.
 connection.connect(function(err) {
